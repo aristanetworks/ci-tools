941de8d2e302ea9ec18c92b0a247ef596180077e
Giuseppe Valente <gvalente@arista.com>
2018-01-05 16:35:38 +0100

    build2slack: review comments

--- build2slack.py
+++ build2slack.py
@@ -11,13 +11,7 @@ import subprocess
 import yaml
 
 
-GERRIT_CHANGE_COMMIT_MESSAGE_ENV = "GERRIT_CHANGE_COMMIT_MESSAGE"
-GERRIT_CHANGE_NUMBER_ENV = "GERRIT_CHANGE_NUMBER"
-GERRIT_CHANGE_URL_ENV = "GERRIT_CHANGE_URL"
-GERRIT_PATCHSET_NUMBER_ENV = "GERRIT_PATCHSET_NUMBER"
-GERRIT_REVISION_ENV = "GERRIT_PATCHSET_REVISION"
 PACKAGE_SETTINGS_FILE = 'package.yml'
-SLACK_WEBHOOK_ENV = "SLACK_WEBHOOK"
 
 
 def getEnv(name):
@@ -26,14 +20,14 @@ def getEnv(name):
   return value
 
 
-def gerritRevision():
-  rev = getEnv(GERRIT_REVISION_ENV)
+def filesInRevision():
+  rev = getEnv("GERRIT_REVISION")
   diff = subprocess.check_output(["git", "diff-tree", "--no-commit-id", "--name-only", "-r", rev])
   return diff.split("\n")
 
 
 def dirs(files):
-  return set([os.path.dirname(f) for f in files])
+  return set(os.path.dirname(f) for f in files)
 
 
 def getNotifiees(files):
@@ -51,11 +45,11 @@ def getNotifiees(files):
 
 
 def notify(failed=False):
-  patchset = getEnv(GERRIT_PATCHSET_NUMBER_ENV)
-  title = "gerrit/%s/%s" % (getEnv(GERRIT_CHANGE_NUMBER_ENV), patchset)
-  url = getEnv(GERRIT_CHANGE_URL_ENV)
+  patchset = getEnv("GERRIT_PATCHSET_NUMBER")
+  title = "gerrit/%s/%s" % (getEnv("GERRIT_CHANGE_NUMBER"), patchset)
+  url = getEnv("GERRIT_CHANGE_URL")
   titleLink = url + "/" + patchset
-  message = base64.b64decode(getEnv(GERRIT_CHANGE_COMMIT_MESSAGE_ENV)).split("\n")[0]
+  message = base64.b64decode(getEnv("GERRIT_CHANGE_COMMIT_MESSAGE")).split("\n")[0]
   payload = {"username": "jenkins",
              "icon_url": "https://a.slack-edge.com/205a/img/services/jenkins-ci_72.png"}
   payload["attachments"] = [{"color": "danger" if failed else "good",
@@ -63,9 +57,8 @@ def notify(failed=False):
                              "title_link": titleLink,
                              "text": message,
                              "mrkdwn_in": ["text"]}]
-  files = gerritRevision()
-  webHook = os.environ.get(SLACK_WEBHOOK_ENV)
-  assert webHook, SLACK_WEBHOOK_ENV + " is not set"
+  files = filesInRevision()
+  webHook = getEnv("SLACK_WEBHOOK")
   for channel in getNotifiees(files):
     payload['channel'] = channel
     requests.post(webHook, data=json.dumps(payload))
